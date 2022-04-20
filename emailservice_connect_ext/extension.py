2df7754c407d432ac6878f7dd5156012edb43e3c
JoseAMaestre <104069157+JoseAMaestre@users.noreply.github.com>
2022-04-20 13:09:19 +0200

    Update extension.py

--- emailservice_connect_ext/extension.py
+++ emailservice_connect_ext/extension.py
@@ -20,20 +20,20 @@ class EmailServiceExtension(Extension):
                 return param
                 
     def process_asset_purchase_request(self, request):
-        #Add an entry to the log
+        # Add an entry to the log
         self.logger.info(f"Obtained request with id {request['id']}")
-        #Get the value for Email parameter
+        # Get the value for Email parameter
         # dest_email_param = self.get_param_by_id(request['asset']['params'], 'Email')
-        #Check if the parameter was empty or not found
+        # Check if the parameter was empty or not found
         # if dest_email_param:
         #     dest_email = dest_email_param['value']
         #     self.logger.info(f"The Email in the request is " + dest_email)
         # else:
         #     self.logger.error("I was not able to find the Email parameter, skipping process")
         #     return ProcessingResponse.done()
-        #Test email config
+        # Test email config
         dest_email = "joseant.maestre@gmail.com"
-        #Start mail processing
+        # Start mail processing
         mail_from = self.config['MAILFROM']
         mail_subject = self.config['MAILSUBJECT']
         mail_html = self.config['HTMLBODY']
@@ -41,9 +41,9 @@ class EmailServiceExtension(Extension):
         username = self.config['USERNAME']
         password = self.config['PASSWORD']
         mimemsg = MIMEMultipart()
-        mimemsg['From']=mail_from
-        mimemsg['To']=dest_email
-        mimemsg['Subject']=mail_subject
+        mimemsg['From'] = mail_from
+        mimemsg['To'] = dest_email
+        mimemsg['Subject'] = mail_subject
         mimemsg.attach(MIMEText(mail_html, 'html'))
         connection = smtplib.SMTP(host=smtp_host, port=587)
         connection.starttls()
